407277df9f3054421d921f160dde34f7f00c4a38
Chen-Yuan-Lai <a186235@gmail.com>
2022-12-08 15:51:36 +0800

    modify

--- deque.h
+++ deque.h
@@ -26,7 +26,7 @@ private:
     public:
         Node(T element = 0) : data(element), left(0), right(0) {}
         T data;
-        Node *right, *left;
+        Node *left, *right;
     };
     Node *first;
 };
